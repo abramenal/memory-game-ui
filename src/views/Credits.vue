19bfa0bc146b00c9d730951fa9e65f7abbec3427
abramenal <pavel.auramenka@gmail.com>
2021-02-27 14:38:40 +0100

    feat: add credits tab

--- src/views/Credits.vue
+++ src/views/Credits.vue
@@ -1,5 +1,26 @@
 <template>
-  credits
+  <ul class="credits">
+    <li>
+      <img
+        class="img"
+        src="../assets/github.svg"
+        width="25"
+        alt="github logo"
+      />
+      Made by
+      <a class="link" href="https://github.com/abramenal" target="_blank">@abramenal</a>
+    </li>
+    <li>
+      <img
+        class="img"
+        src="../assets/vue.svg"
+        width="25"
+        alt="vue.js framework logo"
+      />
+      Powered by
+      <a class="link" href="https://vuejs.org"  target="_blank">Vue.js</a>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -9,5 +30,18 @@ export default {
 </script>
 
 <style>
+.credits {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 
+  list-style-type: none;
+  padding: 0;
+}
+.img {
+  margin-left: 25px;
+}
+.link {
+  color: #06c;
+}
 </style>
