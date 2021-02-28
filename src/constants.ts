af35ce67ffe47946f9a9065733b789f3ab019a65
abramenal <pavel.auramenka@gmail.com>
2021-02-28 02:27:48 +0100

    chore: support ENV variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000';
